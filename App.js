316c55f95b9d92c8e4c3d6b3cfad27ba879f37f4
Alex <aleck.boldareff2014@gmail.com>
2021-08-02 14:26:34 +0300

    added button

--- App.js
+++ App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       {/*  <p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p> */}
+        </p>
+        <button>Test</button>  {/* для примера, потом стиреть */}
         <a
           className="App-link"
           href="https://reactjs.org"
